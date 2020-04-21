fac786e25a666898c2b03d2422069799cd14b645
xqbptt <62851240+xqbptt@users.noreply.github.com>
2020-04-21 22:46:06 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-logo.svg.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-logo.svg.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	let myName = prompt("Please enter your name.");
+	if(!myName || myName===null){
+		setUserName();
+	}
+	else{
+	localStorage.setItem('name',myName);
+	myHeading.textContent = "Mozilla is cool, " + myName;
+	}
+}
+if(!localStorage.getItem('name')){
+	setUserName();
+}
+else
+{
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function(){
+	setUserName();
+}
